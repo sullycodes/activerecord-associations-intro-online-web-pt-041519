b90b482705815b17f2216b9704f0ea7d9d424340
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-13 19:02:10 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -3,7 +3,7 @@ class CreateSongs < ActiveRecord::Migration[4.2]
     create_table :songs do |t|
       t.string :name 
       t.integer :artist_id 
-      t,integer :genre_id 
+      t.integer :genre_id 
     end
   end
 end
